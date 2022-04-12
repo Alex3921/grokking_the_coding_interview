0684f541a7db920b35ac7ee176d8cf64a1c7a1c1
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-11 23:50:24 -0500

    Solved

--- Pattern: Sliding Window/(medium) Longest Substring with maximum K Distinct Characters.js
+++ Pattern: Sliding Window/(medium) Longest Substring with maximum K Distinct Characters.js
@@ -3,3 +3,37 @@
 // in it with no more than K distinct characters.
 
 
+const longest_substring_with_k_distinct = function(str, k) {
+    let windowStart = 0,
+        maxLength = 0,
+        charFrequency = {};
+
+    for(let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        const rightChar = str[windowEnd];
+        if(!(rightChar in charFrequency)) {
+            charFrequency[rightChar] = 0;
+        }
+
+        charFrequency[rightChar] += 1;
+        while(Object.keys(charFrequency).length > k) {
+            const leftChar = str[windowStart];
+            charFrequency[leftChar] -= 1;
+
+            if(charFrequency[leftChar] === 0) {
+                delete charFrequency[leftChar];
+            }
+            windowStart += 1;
+        }
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    }
+    return maxLength;
+  };
+
+
+// Time complexity: O(n)
+// Space complexity: O(k + 1)
+
+
+console.log(`Length of the longest substring: ${longest_substring_with_k_distinct('araaci', 2)}`);
+console.log(`Length of the longest substring: ${longest_substring_with_k_distinct('araaci', 1)}`);
+console.log(`Length of the longest substring: ${longest_substring_with_k_distinct('cbbebi', 3)}`);
