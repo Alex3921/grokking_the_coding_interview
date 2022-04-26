2a07f93bb0106321e57c42a2f95958c7539beb8d
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-25 23:27:21 -0500

    Add minimum meeting rooms

--- merge_intervals/(hard) Minimum meeting rooms.js
+++ merge_intervals/(hard) Minimum meeting rooms.js
@@ -1,7 +1,7 @@
 // Statement:
 // Given a list of intervals representing the start and end time
 // of ‘N’ meetings, find the minimum number of rooms required to hold all the meetings.
-
+const Heap = require('./collections/heap');
 class Meeting {
   constructor(start, end) {
     this.start = start;
@@ -9,12 +9,50 @@ class Meeting {
   }
 }
 
-const min_meeting_rooms = function (meetings) {
-    
-  
+// const min_meeting_rooms = function (meetings) {
+//   const start = [];
+//   const end = [];
+//   let result = 0;
+//   let count = 0;
+//   let s = 0;
+//   let e = 0;
 
-  return roomsCount;
-};
+//   for (let m of meetings) start.push(m.start), end.push(m.end);
+//   start.sort((a, b) => a - b);
+//   end.sort((a, b) => a - b);
+
+//   while (s < meetings.length) {
+//     if (start[s] < end[e]) {
+//       s++;
+//       count++;
+//     } else {
+//       e++;
+//       count--;
+//     }
+//     result = Math.max(result, count);
+//   }
+
+//   return result;
+// };
+
+const min_meeting_rooms = function (meetings) {
+  meetings.sort((a,b) => a.start - b.start)
+  const minHeap = new Heap([], null, ((a,b) => b.end - a.end));
+  let minRooms = 0;
+
+  for(let i = 0; i < meetings.length; i++) {
+    // remove all the meetings that have ended
+    while(minHeap.length > 0 && meetings[i].start >= minHeap.peek().end) {
+      minHeap.pop();
+    }
+
+    // add current meeting into minHeap
+    minHeap.push(meetings[i]);
+    // all active meetings are in the min_heap, so we need rooms for all of them
+    minRooms = Math.max(minRooms, minHeap.length);
+  }
+  return minRooms;
+}
 
 console.log(
   `Minimum meeting rooms required: ${min_meeting_rooms([
