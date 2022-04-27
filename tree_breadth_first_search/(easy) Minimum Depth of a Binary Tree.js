651f632f066b8e62da15f36348b5afe034a6ce5b
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-26 22:20:34 -0500

    Add Minimum depth of a binary tree

---
+++ tree_breadth_first_search/(easy) Minimum Depth of a Binary Tree.js
@@ -0,0 +1,45 @@
+// Find the minimum depth of a binary tree. The minimum depth 
+// is the number of nodes along the shortest path from the root 
+// node to the nearest leaf node.
+
+
+const Deque = require('./collections/deque')
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+const find_minimum_depth = function(root) {
+    if(root === null) {
+        return 0;
+    }
+
+    const queue = new Deque();
+    queue.push(root);
+    let maximumTreeDepth = 0;
+    while(queue.length > 0) {
+        maximumTreeDepth += 1;
+        const levelSize = queue.length;
+        for(let i = 0; i < levelSize; i++) {
+            const currentNode = queue.shift();
+            if(currentNode.left === null &&  currentNode.right === null) {
+                return maximumTreeDepth;
+            }
+            if(currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+            if(currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+    }
+    return maximumTreeDepth;
+}
+
+// Time complexity = O(n)
+// Space complexity = O(n)
