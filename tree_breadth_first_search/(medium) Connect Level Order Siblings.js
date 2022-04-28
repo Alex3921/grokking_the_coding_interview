ac7217310a5f3c90cdca9bdd5fb14aeb40008b86
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-28 13:37:21 -0500

    Add connect level order siblings

---
+++ tree_breadth_first_search/(medium) Connect Level Order Siblings.js
@@ -0,0 +1,72 @@
+// Given a binary tree, connect each node with its level order successor.
+// The last node of each level should point to a null node.
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.next = null;
+  }
+
+  // level order traversal using 'next' pointer
+  print_level_order() {
+    console.log("Level order traversal using 'next' pointer: ");
+    let nextLevelRoot = this;
+    while (nextLevelRoot !== null) {
+      let current = nextLevelRoot;
+      nextLevelRoot = null;
+      while (current != null) {
+        process.stdout.write(`${current.val} `);
+        if (nextLevelRoot === null) {
+          if (current.left !== null) {
+            nextLevelRoot = current.left;
+          } else if (current.right !== null) {
+            nextLevelRoot = current.right;
+          }
+        }
+        current = current.next;
+      }
+      console.log();
+    }
+  }
+}
+
+const connect_level_order_siblings = function (root) {
+  // TODO: Write your code here
+  if (root === null) {
+    return 0;
+  }
+
+  const queue = new Deque();
+  queue.push(root);
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    for (let i = 0; i < levelSize; i++) {
+      const currentNode = queue.shift();
+      currentNode.next = queue.peek();
+      if (i === levelSize - 1) {
+        currentNode.next = null;
+      }
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+    }
+  }
+};
+
+//   Time complexity: O(n)
+//   Space complexity: O(n)
+
+var root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+connect_level_order_siblings(root);
+
+root.print_level_order();
