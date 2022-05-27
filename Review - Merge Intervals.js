f3b9585e49fff5581384472cfb841cabfbfa6c2a
Alexandru Boncut <boncut.alex10@gmail.com>
2022-05-26 21:41:31 -0500

    Add rest of challenges

--- Review - Merge Intervals.js
+++ Review - Merge Intervals.js
@@ -182,19 +182,17 @@
 // }
 // console.log();
 
-
-
 // class Interval {
 //     constructor(start, end) {
 //       this.start = start;
 //       this.end = end;
 //     }
-  
+
 //     print_interval() {
 //       process.stdout.write(`[${this.start}, ${this.end}]`);
 //     }
 //   }
-  
+
 //   const can_attend_all_appointments = function(intervals) {
 //     if(intervals.length < 2) return true;
 //     intervals.sort((a, b) => a.start - b.start);
@@ -208,23 +206,128 @@
 //     }
 //     return true;
 //   };
-  
-  
+
 //   console.log(`Can attend all appointments: ${can_attend_all_appointments([
 //     new Interval(1, 4),
 //     new Interval(2, 5),
 //     new Interval(7, 9),
 //   ])}`);
-  
+
 //   console.log(`Can attend all appointments: ${can_attend_all_appointments([
 //     new Interval(6, 7),
 //     new Interval(2, 4),
 //     new Interval(8, 12),
 //   ])}`);
-  
+
 //   console.log(`Can attend all appointments: ${can_attend_all_appointments([
 //     new Interval(1, 2),
 //     new Interval(2, 3),
 //     new Interval(3, 6),
 //   ])}`);
-  
+
+// class Interval {
+//   constructor(start, end) {
+//     this.start = start;
+//     this.end = end;
+//   }
+
+//   print_interval() {
+//     process.stdout.write(`[${this.start}, ${this.end}]`);
+//   }
+// }
+
+// const can_attend_all_appointments = function (intervals) {
+//   if (intervals.length < 2) return true;
+//   intervals.sort((a, b) => a.start - b.start);
+//   const result = [];
+//   let maxEndInterval = intervals[0];
+
+//   for (let i = 1; i < intervals.length; i++) {
+//     let current = intervals[i];
+//     if (current.start < maxEndInterval.end) {
+//       result.push(current);
+//       result.push(maxEndInterval);
+//     }
+//     if (current.end > maxEndInterval.end) {
+//       maxEndInterval = current;
+//     }
+//   }
+//   return result;
+// };
+
+// let merged_intervals = can_attend_all_appointments([
+//   new Interval(4, 5),
+//   new Interval(2, 3),
+//   new Interval(3, 6),
+//   new Interval(5, 7),
+//   new Interval(7, 8),
+// ]);
+// for (let i = 0; i < merged_intervals.length; i++) {
+//   console.log(merged_intervals[i]);
+// }
+
+// import Heap from "collections/heap.js";
+// class Meeting {
+//   constructor(start, end) {
+//     this.start = start;
+//     this.end = end;
+//   }
+// }
+
+// function min_meeting_rooms(meetings) {
+//   if (meetings.length < 2) return meetings;
+//   meetings.sort((a, b) => a.start - b.start);
+//   const minHeap = new Heap([], null, (a, b) => b.end - a.end);
+//   let minRooms = 0;
+
+//   for (let i = 0; i < meetings.length; i++) {
+//     const currentMeeting = meetings[i];
+//     while (minHeap.length > 0 && currentMeeting.start >= minHeap.peek().end) {
+//       minHeap.pop();
+//     }
+
+//     minHeap.push(currentMeeting);
+//     minRooms = Math.max(minRooms, minHeap.length);
+//   }
+//   return minRooms;
+// }
+
+// console.log(
+//   `Minimum meeting rooms required: ${min_meeting_rooms([
+//     new Meeting(4, 5),
+//     new Meeting(2, 3),
+//     new Meeting(2, 4),
+//     new Meeting(3, 5),
+//   ])}`
+// );
+// console.log(
+//   `Minimum meeting rooms required: ${min_meeting_rooms([
+//     new Meeting(1, 4),
+//     new Meeting(2, 5),
+//     new Meeting(7, 9),
+//   ])}`
+// );
+// console.log(
+//   `Minimum meeting rooms required: ${min_meeting_rooms([
+//     new Meeting(6, 7),
+//     new Meeting(2, 4),
+//     new Meeting(8, 12),
+//   ])}`
+// );
+// console.log(
+//   `Minimum meeting rooms required: ${min_meeting_rooms([
+//     new Meeting(1, 4),
+//     new Meeting(2, 3),
+//     new Meeting(3, 6),
+//   ])}`
+// );
+// console.log(
+//   `Minimum meeting rooms required: ${min_meeting_rooms([
+//     new Meeting(4, 5),
+//     new Meeting(2, 3),
+//     new Meeting(2, 4),
+//     new Meeting(3, 5),
+//   ])}`
+// );
+
+
